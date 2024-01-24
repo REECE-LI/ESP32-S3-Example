bc7d9d665dfe3f6fa7363dbb25245d06be4cebff
XiaoJun <2248246525@qq.com>
2024-01-24 12:45:22 +0800

    添加了CAN的基础库 以及shell命令

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
+#include "can_esp.h"
+#include "shellfunc.h"
 
 void setup() {
 // write your initialization code here
     Serial.begin(115200);
+    shell.attach(Serial);
+    shellInit();
 }
 
 void loop() {
 // write your code here
-    Serial.println("Hello World!");
-    delay(1);
+//    Serial.println("Hello World!");
+    shell.executeIfInput();
+    delay(10);
 }
